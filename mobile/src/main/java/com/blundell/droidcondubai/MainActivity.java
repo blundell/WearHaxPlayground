56c367204175216d1b04853ebf73e33036d40954
Blundell <blundell.app.dev@gmail.com>
2015-04-16 17:08:04 +0400

    Captures reply from wear in activity

--- mobile/src/main/java/com/blundell/droidcondubai/MainActivity.java
+++ mobile/src/main/java/com/blundell/droidcondubai/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -31,6 +32,11 @@ public class MainActivity extends ActionBarActivity {
                     }
                 });
 
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(getIntent());
+        if (remoteInput != null) {
+            CharSequence reply = remoteInput.getCharSequence(KEY_REPLY);
+            Toast.makeText(this, reply, Toast.LENGTH_LONG).show();
+        }
     }
 
     private Notification buildNotification() {
