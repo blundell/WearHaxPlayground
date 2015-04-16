719bf48bb1c4d339059ae10ba84563a9ee4a284c
Blundell <blundell.app.dev@gmail.com>
2015-04-16 18:22:33 +0400

    canvas watchface BOOOOOM

--- wear/src/main/java/com/blundell/droidcondubai/WatchFace.java
+++ wear/src/main/java/com/blundell/droidcondubai/WatchFace.java
@@ -1,6 +1,11 @@
 package com.blundell.droidcondubai;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.text.format.Time;
 import android.view.SurfaceHolder;
 
 public class WatchFace extends CanvasWatchFaceService {
@@ -12,9 +17,30 @@ public class WatchFace extends CanvasWatchFaceService {
 
     private class Engine extends CanvasWatchFaceService.Engine {
 
+        private Time time;
+        private Paint backgroundPaint;
+        private Paint textPaint;
+
         @Override
         public void onCreate(SurfaceHolder holder) {
-            super.onCreate(holder);
+            time = new Time();
+
+            backgroundPaint = new Paint();
+            backgroundPaint.setColor(Color.WHITE);
+            backgroundPaint.setAntiAlias(true);
+
+            textPaint = new Paint();
+            textPaint.setColor(Color.BLUE);
+            textPaint.setAntiAlias(true);
+            textPaint.setTextSize(20F);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            time.setToNow();
+            String timeFormatted = time.hour + ":" + time.minute;
+            canvas.drawCircle(bounds.centerX(), bounds.centerY(), 100, backgroundPaint);
+            canvas.drawText(timeFormatted, bounds.centerX() - 20, bounds.centerY(), textPaint);
         }
     }
 }
